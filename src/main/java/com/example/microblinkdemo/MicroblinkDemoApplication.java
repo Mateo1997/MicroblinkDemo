9caebd197c7cca6f5f8dc4c1e2efba9eae93f119
Mateo Miličević <mateo.milicevic@trilix.eu>
2022-08-25 12:01:00 +0200

    Init

---
+++ src/main/java/com/example/microblinkdemo/MicroblinkDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.microblinkdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroblinkDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroblinkDemoApplication.class, args);
+    }
+
+}
