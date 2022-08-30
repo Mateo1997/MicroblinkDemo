76ede5fa995bfcff45c290333fe0dbce3a876aae
Mateo Miličević <mateo.milicevic@trilix.eu>
2022-08-30 22:26:44 +0200

    change - move microblinkException method to RetrofitHelper class and delete redundant Helper class

--- src/main/java/com/example/microblinkdemo/retrofit/RetrofitHelper.java
+++ src/main/java/com/example/microblinkdemo/retrofit/RetrofitHelper.java
@@ -1,19 +1,26 @@
 package com.example.microblinkdemo.retrofit;
 
+import com.example.microblinkdemo.exception.MicroblinkException;
 import com.example.microblinkdemo.exception.ServiceUnavailableException;
 import com.example.microblinkdemo.mrz.api.MrtdResponseExternal;
 import com.example.microblinkdemo.mrz.domain.MrtdRequest;
-import com.example.microblinkdemo.util.Helper;
+import com.example.microblinkdemo.retrofit.model.MicroblinkErrorResponse;
 import com.example.microblinkdemo.util.ResponseConstants;
+import com.google.gson.Gson;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import retrofit2.Response;
+
+import java.io.IOException;
+
+import static com.example.microblinkdemo.util.ResponseConstants.ERROR_MICROBLINK_SERVICE_INVALID_RESPONSE;
 
 @Component
 @RequiredArgsConstructor
 public class RetrofitHelper {
 
     private final MicroblinkAPI microblinkAPI;
+    private static final Gson gson = new Gson();
 
     public String getRawMrzString(MrtdRequest request) {
         Response<MrtdResponseExternal> response;
@@ -31,6 +38,20 @@ public class RetrofitHelper {
                     .getRawMrzString();
         }
 
-        throw Helper.microblinkException(response);
+        throw microblinkException(response);
+    }
+
+    private <T> RuntimeException microblinkException(Response<T> response) {
+        if (response != null && response.errorBody() != null) {
+            String errorBody;
+            try {
+                errorBody = response.errorBody().string();
+            } catch (IOException e) {
+                return new MicroblinkException(ERROR_MICROBLINK_SERVICE_INVALID_RESPONSE, e);
+            }
+            MicroblinkErrorResponse e = gson.fromJson(errorBody, MicroblinkErrorResponse.class);
+            return new MicroblinkException(e.getCode());
+        }
+        return new MicroblinkException(ERROR_MICROBLINK_SERVICE_INVALID_RESPONSE);
     }
 }
